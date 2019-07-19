92f26c6d03eae9c813cf478028ba1ed789a20d45
Phone Nyo <phonenai2014@gmail.com>
2019-07-19 11:09:12 +0630

    Defined channel & laravel-echo

--- routes/channels.php
+++ routes/channels.php
@@ -14,3 +14,11 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+// As we aren’t using Laravel auth, we return true in the function callback 
+// so that everybody can use this channel to broadcast events.
+Broadcast::channel('newTask', function(){
+    return true;
+});
+Broadcast::channel('taskRemoved', function(){
+    return true;
+});
